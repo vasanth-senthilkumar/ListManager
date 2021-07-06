577f5d20311b05a82e5d5f2b238ab61bcf65680b
vasanth-senthilkumar <vasanthfourbright@gmail.com>
2021-07-06 21:07:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+import "./App.css";
+
+import NotFound from "./components/not-found/NotFound";
+import Profile from "./components/profile/Profile";
+import Profiles from "./components/profiles/Profiles";
+import AddEducation from "./components/add-credentials/AddEducation";
+import AddExperience from "./components/add-credentials/AddExperience";
+import EditProfile from "./components/edit-profile/EditProfile";
+import CreateProfile from "./components/create-profile/CreateProfile";
+import PrivateRoute from "./components/common/PrivateRoute";
+import Navbar from "../src/components/layout/Navbar";
+import Footer from "../src/components/layout/Footer";
+import Landing from "../src/components/layout/Landing";
+import Login from "../src/components/auth/Login";
+import Register from "../src/components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+import { clearCurrentProfile } from "./actions/profileActions";
+
+if (localStorage.jwt) {
+  setAuthToken(localStorage.jwt);
+  const decoded = jwt_decode(localStorage.jwt);
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    store.dispatch(clearCurrentProfile());
+    window.location.href = "/login";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
+              </Switch>
+
+              <Route exact path="/not-found" component={NotFound} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
